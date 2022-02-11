69f23c2219c2172b0fcd1deb1adb9e0c76a9c131
tarotech96 <nguyenvancong4696@gmail.com>
2022-02-11 19:32:57 +0900

    make it better

--- src/components/main/blog/CardBlog.tsx
+++ src/components/main/blog/CardBlog.tsx
@@ -13,7 +13,7 @@ const CardBlog: React.FC<{ blog: IBlog }> = ({ blog }) => {
         alt={blog.title}
         onClick={() => handleClick(blog.url)}
       />
-      <h4 className="text-[#e4ee89] text-base font-bold ml-4">
+      <h4 className="text-[#e4ee89] text-base font-bold ml-4 mt-[1rem] break-all">
         {blog.author ?? "Anonymous"}
       </h4>
       <p className="text-pink-300 text-sm break-all ml-4 mt-4 w-[90%]">
